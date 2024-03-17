6c5623a3bf5499fd856d92261184c43738cc9a60
Daria Ponikarenko <daria.ponikarenko@gmail.com>
2024-03-17 21:18:49 +0300

    Test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import axios from "axios";
 import "./App.css";
@@ -19,9 +18,6 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>{title}</h1>
