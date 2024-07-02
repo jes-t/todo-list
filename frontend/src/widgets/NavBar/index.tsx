a8d6560d6b358b5e4beb346b4ed39559c74067a3
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2024-07-02 02:35:19 +0200

    Change color

--- frontend/src/widgets/NavBar/index.tsx
+++ frontend/src/widgets/NavBar/index.tsx
@@ -1,36 +1,37 @@
-import styled from 'styled-components';
-import { NavLink } from './ui/NavLink';
+import styled from "styled-components";
+import { NavLink } from "./ui/NavLink";
 
 export function NavBar() {
-	return (
-		<Root>
-			<Brand>RightPlan</Brand>
-			<ManageLinks>
-				<NavLink to="/" title="To-do" />
-				<NavLink to="/profile" title="Profile" />
-			</ManageLinks>
-		</Root>
-	);
+  return (
+    <Root>
+      <Brand>RightPlan</Brand>
+      <ManageLinks>
+        <NavLink to="/" title="To-do" />
+        <NavLink to="/profile" title="Profile" />
+      </ManageLinks>
+    </Root>
+  );
 }
 
 const Brand = styled.span`
-	font-weight: bold;
-	margin-bottom: 30px;
+  font-weight: bold;
+  margin-bottom: 30px;
+  color: red;
 `;
 const Root = styled.div`
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-	width: 200px;
-	min-width: 200px;
-	border-right: 1px solid black;
-	padding: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 200px;
+  min-width: 200px;
+  border-right: 1px solid black;
+  padding: 20px;
 `;
 const ManageLinks = styled.ul`
-	width: 100%;
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-	gap: 10px;
-	list-style-type: none;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+  list-style-type: none;
 `;
